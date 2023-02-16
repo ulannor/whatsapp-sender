a047682986b591e9793e801437cff5c0f728a44e
WintBree <u.duulatov@gmail.com>
2023-02-17 02:03:11 +0600

    sender functions added

--- stage_2_mailout.py
+++ stage_2_mailout.py
@@ -5,7 +5,7 @@ messagetxt = 'text'
 n = 20
 
 filepath = f'.\\edited\\{leadsfile}.xlsx'
-txtpath =  f'.\\messages\\{messagetxt}.txt'
+txtpath = f'.\\messages\\{messagetxt}.txt'
 
 df = pd.read_excel(filepath, dtype='object')
 
@@ -16,21 +16,30 @@ nonwaNumberColList = [col for col in df if col.startswith('phone')]
 
 try:
     for _ in range(0, n):
+        walist = []
+
         for col in waNumberColList:
             phone = df.iloc[_][col]
             if pd.isna(phone):
                 break
             print(phone)
+            print('waphone')
             print(type(phone))
+            walist.append(phone)
             print(df.iloc[_]['address name'])
+
         for col in nonwaNumberColList:
             phone = df.iloc[_][col]
             if pd.isna(phone):
                 break
             elif phone.startswith('+996312'):
                 break
+            elif phone in walist:
+                break
             print(phone)
+            print('simphone')
             print(type(phone))
             print(df.iloc[_]['address name'])
+
 except IndexError as e:
-  print(e)
+    print(e)
