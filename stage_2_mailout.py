f2278f42c34c9af22c3742a922574931a9ecc313
WintBree <u.duulatov@gmail.com>
2023-03-23 02:41:05 +0600

    edited comments

--- stage_2_mailout.py
+++ stage_2_mailout.py
@@ -1,30 +1,32 @@
 import pandas as pd
 import sender_functions as sf
 
+# file names and parameters
+leadsfile = 'нотариальные услуги_edited'   # name of the leads file
+messagetxt = 'text'                        # name of the message text file
+n = 120                                    # number of rows to process in the leads file
 
-leadsfile = 'нотариальные услуги_edited'
-messagetxt = 'text'
-logfile = 'wa_mailout_log'
+# file paths
+LEADSFILEPATH = f'.\\edited\\{leadsfile}.xlsx'  # path to the leads file
+MSGPATH = f'.\\messages\\{messagetxt}.txt'  # path to the message text file
+LOGPATH = f'.\\wa_mailout_log\\wa_mailout_log.xlsx'  # path to the log file
 
-n = 120
+# read the leads file into a pandas dataframe
+df = pd.read_excel(LEADSFILEPATH, dtype='object')
 
-filepath = f'.\\edited\\{leadsfile}.xlsx'
-txtpath = f'.\\messages\\{messagetxt}.txt'
-logpath = f'.\\wa_mailout_log\\{logfile}.xlsx'
-
-df = pd.read_excel(filepath, dtype='object')
-
-
+# get the list of columns containing WhatsApp numbers and non-WhatsApp numbers
 waNumberColList = [col for col in df if col.startswith('whatsapp')]
 nonwaNumberColList = [col for col in df if col.startswith('phone')]
 
-msg = sf.read_txt_file(txtpath)
+# read the message text from the text file
+msg = sf.read_txt_file(MSGPATH)
 
-
+# iterate over each row in the leads file, sending messages to WhatsApp and non-WhatsApp numbers
 try:
     for _ in range(0, n):
         walist = []
 
+        # iterate over columns containing WhatsApp numbers
         for col in waNumberColList:
             phone = df.iloc[_][col]
             if pd.isna(phone):
@@ -39,15 +41,16 @@ try:
             phone = sf.format_phone(phone)
             print(phone)
             sf.send_msg(phone, msg)
-            sf.logwriter(dftemp, logpath)
+            sf.logwriter(dftemp, LOGPATH)
 
+        # iterate over columns containing non-WhatsApp numbers
         for col in nonwaNumberColList:
             phone = df.iloc[_][col]
             if pd.isna(phone):
                 break
-            elif phone.startswith('+996312'):
+            elif phone.startswith('+996312'):  # ignore phone numbers starting with +996312
                 break
-            elif phone in walist:
+            elif phone in walist:  # ignore phone numbers that have already been sent a WhatsApp message
                 break
             dftemp = df.iloc[_, 0:11]
             dftemp['message_wanumber'] = ''
@@ -58,7 +61,7 @@ try:
             phone = sf.format_phone(phone)
             print(phone)
             sf.send_msg(phone, msg)
-            sf.logwriter(dftemp, logpath)
+            sf.logwriter(dftemp, LOGPATH)
 
 except IndexError as e:
     print(e)
