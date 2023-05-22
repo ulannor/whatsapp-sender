0c4e35c5b1dc6f00d9d391ab506061f0317cafae
UlanDev <116745669+ulannor@users.noreply.github.com>
2023-05-22 14:55:15 +0600

    Update stage_2_mailout.py
    removed redundant try block

--- stage_2_mailout.py
+++ stage_2_mailout.py
@@ -22,46 +22,42 @@ nonwaNumberColList = [col for col in df if col.startswith('phone')]
 msg = sf.read_txt_file(MSGPATH)
 
 # iterate over each row in the leads file, sending messages to WhatsApp and non-WhatsApp numbers
-try:
-    for _ in range(0, n):
-        walist = []
+for _ in range(0, n):
+    walist = []
 
-        # iterate over columns containing WhatsApp numbers
-        for col in waNumberColList:
-            phone = df.iloc[_][col]
-            if pd.isna(phone):
-                break
-            walist.append(phone)
-            dftemp = df.iloc[_, 0:11]
-            dftemp['message_wanumber'] = phone
-            dftemp['message_nonwanumber'] = ''
-            dftemp['date_and_time'] = pd.Timestamp.now().strftime("%d/%m/%Y, %H:%M:%S")
-            dftemp['message_text'] = msg
-            dftemp = dftemp.to_frame()
-            phone = sf.format_phone(phone)
-            print(phone)
-            sf.send_msg(phone, msg)
-            sf.logwriter(dftemp, LOGPATH)
+    # iterate over columns containing WhatsApp numbers
+    for col in waNumberColList:
+        phone = df.iloc[_][col]
+        if pd.isna(phone):
+            break
+        walist.append(phone)
+        dftemp = df.iloc[_, 0:11]
+        dftemp['message_wanumber'] = phone
+        dftemp['message_nonwanumber'] = ''
+        dftemp['date_and_time'] = pd.Timestamp.now().strftime("%d/%m/%Y, %H:%M:%S")
+        dftemp['message_text'] = msg
+        dftemp = dftemp.to_frame()
+        phone = sf.format_phone(phone)
+        print(phone)
+        sf.send_msg(phone, msg)
+        sf.logwriter(dftemp, LOGPATH)
 
-        # iterate over columns containing non-WhatsApp numbers
-        for col in nonwaNumberColList:
-            phone = df.iloc[_][col]
-            if pd.isna(phone):
-                break
-            elif phone.startswith('+996312'):  # ignore phone numbers starting with +996312
-                break
-            elif phone in walist:  # ignore phone numbers that have already been sent a WhatsApp message
-                break
-            dftemp = df.iloc[_, 0:11]
-            dftemp['message_wanumber'] = ''
-            dftemp['message_nonwanumber'] = phone
-            dftemp['date_and_time'] = pd.Timestamp.now().strftime("%d/%m/%Y, %H:%M:%S")
-            dftemp['message_text'] = msg
-            dftemp = dftemp.to_frame()
-            phone = sf.format_phone(phone)
-            print(phone)
-            sf.send_msg(phone, msg)
-            sf.logwriter(dftemp, LOGPATH)
-
-except IndexError as e:
-    print(e)
+    # iterate over columns containing non-WhatsApp numbers
+    for col in nonwaNumberColList:
+        phone = df.iloc[_][col]
+        if pd.isna(phone):
+            break
+        elif phone.startswith('+996312'):  # ignore phone numbers starting with +996312
+            break
+        elif phone in walist:  # ignore phone numbers that have already been sent a WhatsApp message
+            break
+        dftemp = df.iloc[_, 0:11]
+        dftemp['message_wanumber'] = ''
+        dftemp['message_nonwanumber'] = phone
+        dftemp['date_and_time'] = pd.Timestamp.now().strftime("%d/%m/%Y, %H:%M:%S")
+        dftemp['message_text'] = msg
+        dftemp = dftemp.to_frame()
+        phone = sf.format_phone(phone)
+        print(phone)
+        sf.send_msg(phone, msg)
+        sf.logwriter(dftemp, LOGPATH)
